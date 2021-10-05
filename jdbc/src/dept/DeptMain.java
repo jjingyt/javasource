70c50fc7a6c0a3f7887a98267805849b2271deda
kyung <jjingyt@naver.com>
2021-10-06 08:41:18 +0900

    메인메뉴화면 작성

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -3,7 +3,11 @@ package dept;
 public class DeptMain {
 
 	public static void main(String[] args) {
-		
+		boolean run  = true;
+		Scanner sc = new Scanner(System.in);
+		while(run) {
+			
+		}
 		
 	}
 
