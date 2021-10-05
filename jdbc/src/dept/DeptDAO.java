1a01f60329ad63045d6ad7c3a76093659c63fcba
kyung <jjingyt@naver.com>
2021-10-06 07:34:23 +0900

    update 추가

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -133,7 +133,35 @@ public class DeptDAO {
 		return flag;
 	}
 
+	// 부서명 변경: update dept_temp set dname=? where deptno=?
+	public boolean update(String dname,int deptNo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		boolean flag = false;
 		
+		try {
+			String sql = "update dept_temp set dname=? where deptno=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, dname);
+			pstmt.setInt(2, deptNo);
+			
+			int result = pstmt.executeUpdate();
+			if (result>0) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				pstmt.close();
+				con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+			
+		}
+		return flag;
+	}
 	
 }
 
